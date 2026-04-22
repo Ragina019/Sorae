9637daca858b372f55ce620c8a33ea33c966cf45
Ragina019 <raginamelin019@gmail.com>
2026-04-22 13:27:12 +0630

    Update comic_settings.js

--- src/rarebit/js/comic_settings.js
+++ src/rarebit/js/comic_settings.js
@@ -40,7 +40,7 @@ if (pg == 0) {pg = maxpg;} //display MOST RECENT COMIC when the webpage is loade
     title: "Look After - Page 1",
     date: "2026-04-22",
     altText: "A description of the art",
-    imageFiles: ["Look-After/page1.png"] 
+    imageFiles: ["img/comics/Look-After/page1.png"] 
 },
 */
 //Note: the formatting is important! The whole thing won't show up if you forget to include the commas or curly braces in the right place.
